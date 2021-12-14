849c3655eaa10c42debd4fa39e72ea0d4769c36b
Sanowarsunny <85189283+Sanowarsunny@users.noreply.github.com>
2021-12-14 12:37:53 +0600

    Delete bloodhub/view directory

--- bloodhub/view/file.php
+++
@@ -1,9 +0,0 @@
-<?php
-$myfile = fopen("sign.txt", "a+") or die("Unable to open file!");
-// Output one line until end-of-file
-while(!feof($myfile)) {
-  echo fgets($myfile) . "<br>";
-}
-fclose($myfile);
-?>
-
